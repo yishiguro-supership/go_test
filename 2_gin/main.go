350107f252b8cf3d002d439685a5e4edea96c625
yuya.ishiguro <yuya.ishiguro@supership.jp>
2024-06-27 16:28:29 +0900

    2_gin リンク追加

--- 2_gin/main.go
+++ 2_gin/main.go
@@ -7,10 +7,35 @@ import (
 
 func main() {
     engine := gin.Default()
+
+    // 実行する関数を直接記載
     engine.GET("/", func(c *gin.Context) {
         c.JSON(http.StatusOK, gin.H{
-            "message": "hello world",
+            "message": "Hello, world!",
         })
     })
+
+    // 別で記載した関数を呼び出し
+    engine.GET("/hello", func(c *gin.Context) { HelloWithoutParam(c) })
+    engine.GET("/hello/:name", func(c *gin.Context) { HelloWithParam(c) })
+
     engine.Run(":3000")
+}
+
+// クエリを利用
+func HelloWithoutParam(c *gin.Context) {
+    name := c.DefaultQuery("name", "NoName")
+    message := c.Query("message")
+    c.JSON(http.StatusOK, gin.H{
+        "message": "Hello, " + name + "!" + message,
+    })
+}
+
+// パスの一部分をパラメータとして利用
+func HelloWithParam(c *gin.Context) {
+    name := c.Param("name")
+    message := c.Query("message")
+    c.JSON(http.StatusOK, gin.H{
+        "message": "Hello, " + name + "!" + message,
+    })
 }
